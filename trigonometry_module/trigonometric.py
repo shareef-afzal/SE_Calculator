f5b0098243f242a1f23e1c7568091550072bf3fb
Aratrika Gupta <157571977+Aratrika-Gupta@users.noreply.github.com>
2026-03-29 22:37:13 +0530

    Add files via upload

--- trigonometry_module/trigonometric.py
+++ trigonometry_module/trigonometric.py
@@ -13,19 +13,32 @@ class Trigonometric:
         return math.cos(self.to_radians(x))
 
     def tan(self, x):
+        x = float(x)
+
+          # tan is undefined at 90 + k*180
+        if x % 180 == 90:
+           raise ValueError("tan undefined at 90 + k*180")
+
         return math.tan(self.to_radians(x))
 
-    def asin(self, x):
+    def asin_domain(self, x):
         x = float(x)
         if x < -1 or x > 1:
-            raise DomainError("asin domain is [-1, 1]")
+            raise ValueError("asin domain is [-1,1]")
         return math.degrees(math.asin(x))
 
-    def acos(self, x):
+    def acos_domain(self, x):
         x = float(x)
         if x < -1 or x > 1:
-            raise DomainError("acos domain is [-1, 1]")
+            raise ValueError("acos domain is [-1,1]")
         return math.degrees(math.acos(x))
+
+
+    def asin_value(self, x):
+        return math.degrees(math.asin(float(x)))
+
+    def acos_value(self, x):
+        return math.degrees(math.acos(float(x)))
 
     def atan(self, x):
         return math.degrees(math.atan(float(x)))
@@ -38,3 +51,4 @@ class Trigonometric:
 
     def tanh(self, x):
         return math.tanh(float(x))
+    
